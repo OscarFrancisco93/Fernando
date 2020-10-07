6ac996bc1546e848a5d4a2a710d0ae47647fc66b
OscarFrancisco93 <72525127+OscarFrancisco93@users.noreply.github.com>
2020-10-07 16:53:22 -0500

    Agregué algunas modificaciones

--- HolaMundo2.c
+++ HolaMundo2.c
@@ -4,5 +4,8 @@ printf("\nFernando Julian Diaz Vazquez\n");
 printf("Este es mi segundo codigo");
 printf("Programar es facil");
 printf("Voy a compartir con mi compañero el codigo");
+printf("Oscar Francisco");
+  
+return 0;
 return 0;
 }
